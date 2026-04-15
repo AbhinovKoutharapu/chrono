a80439131ab4e6dcd5eafb1d023cef2c18b20668
Abhinov Koutharapu <abhinovkoutharapu@mac.lan>
2026-04-15 01:40:17 -0500

    Benchmak tests

---
+++ src/tests/benchmark_tests/geometry/btest_ChDelaunay.cpp
@@ -0,0 +1,48 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+
+#include <benchmark/benchmark.h>
+#include <vector>
+#include <random>
+
+#include "chrono/geometry/ChDelaunay.h"
+#include "chrono/geometry/ChDelaunay.cpp"
+
+using namespace chrono;
+
+// Helper to generate a cloud of random points
+static std::vector<ChVector3d> GenerateRandomPoints(int n) {
+    std::vector<ChVector3d> points;
+    std::mt19937 gen(42);
+    std::uniform_real_distribution<> dis(-10.0, 10.0);
+    for (int i = 0; i < n; ++i) {
+        points.emplace_back(dis(gen), dis(gen), dis(gen));
+    }
+    return points;
+}
+
+// Benchmark function
+static void BM_Delaunay3D(benchmark::State& state) {
+    auto points = GenerateRandomPoints((int)state.range(0));
+
+    for (auto _ : state) {
+        auto tets = ChDelaunay3D::CreateTetrahedralization(points);
+        benchmark::DoNotOptimize(tets);
+    }
+
+    state.SetItemsProcessed(state.iterations() * state.range(0));
+}
+
+// Register tests: starting from 100 points up to 10,000
+BENCHMARK(BM_Delaunay3D)->RangeMultiplier(10)->Range(10, 10000)->Unit(benchmark::kMillisecond);
+
+BENCHMARK_MAIN();
